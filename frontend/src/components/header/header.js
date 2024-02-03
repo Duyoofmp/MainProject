58b01b9d68a1d24872cc722378fa8562ad27bbe4
ASWATHI-123 <aswathishajithpalat@gmail.com>
2024-02-03 10:40:32 +0530

    added

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import '../header/header.css';
+import logo1 from 'file:///C:/react-test/react-test/src/assets/images/logo1.svg';
+
+
 
 
 const Header =()=>{
-    return
+    return(
+        
+        <header>
+            <div className='container-fluid'>
+                <div className='row'>
+                    <div className='col-sm-2'>
+                        <img src={logo1}/>
+                    </div>
+
+                    {/*headerSearch start here */}
+                    <div className='col-sm-5'>
+                        <div className='headerSearch d-flex align-items-center'>
+                            <div className='selectDrop cursor'>
+                            All Categories
+                            </div>
+
+                            <div className='search'>
+                                <input type='text' placeholder='Search for items...'/>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
 
 }
-
+export default Header;
