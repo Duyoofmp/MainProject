c9f8e710cf68d98b88c30dbe2fb293603a04d7ea
ASWATHI-123 <aswathishajithpalat@gmail.com>
2024-02-07 14:23:42 +0530

    header added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import Header from './components/header/header';
+import Select from './components/selectDrop/select';
 
 
 
 function App(){
   return(
     <>
-      <Header></Header>
+      <Header>
+      <Select></Select>
+      </Header>
+      
     </>
   )
 }
--- frontend/src/components/selectDrop/select.js
+++ frontend/src/components/selectDrop/select.js
@@ -1,8 +1,16 @@
 import React from "react";
+import '../selectDrop/select.css';
 
 
 const Select =()=>{
     return(
-        <div className="selectDrop"
+        <div className="selectDrop">
+            <div className="searchField">
+                
+            
+            </div>
+
+        </div>
     )
 }
+export default Select;
