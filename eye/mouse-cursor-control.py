abdea7aea55eefe69a65472d50b8083ed9836be4
Duyoofmp <duyoofibnudileep@gmail.com>
2024-03-14 00:09:49 +0530

    site updated

--- eye/mouse-cursor-control.py
+++ eye/mouse-cursor-control.py
@@ -8,17 +8,15 @@ import dlib
 import cv2
 import bz2
 def mouse():
-    # Thresholds and consecutive frame length for triggering the mouse action.
+   
     MOUTH_AR_THRESH = 0.6
     MOUTH_AR_CONSECUTIVE_FRAMES = 15
     EYE_AR_THRESH = 0.19
     EYE_AR_CONSECUTIVE_FRAMES = 15
     WINK_AR_DIFF_THRESH = 0.04
     WINK_AR_CLOSE_THRESH = 0.19
-    WINK_CONSECUTIVE_FRAMES = 10
+    WINK_CONSECUTIVE_FRAMES = 5
 
-    # Initialize the frame counters for each action as well as 
-    # booleans used to indicate if action is performed or not
     MOUTH_COUNTER = 0
     EYE_COUNTER = 0
     WINK_COUNTER = 0
@@ -35,20 +33,17 @@ def mouse():
     BLUE_COLOR = (255, 0, 0)
     BLACK_COLOR = (0, 0, 0)
 
-    # Initialize Dlib's face detector (HOG-based) and then create
-    # the facial landmark predictor
     shape_predictor_path = "./shape_predictor_68_face_land.dat"
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor(shape_predictor_path)
 
-    # Grab the indexes of the facial landmarks for the left and
-    # right eye, nose and mouth respectively
+   
     (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
     (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
     (nStart, nEnd) = face_utils.FACIAL_LANDMARKS_IDXS["nose"]
     (mStart, mEnd) = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
 
-    # Video capture
+   
     vid = cv2.VideoCapture(0)
     resolution_w = 1366
     resolution_h = 768
@@ -58,18 +53,15 @@ def mouse():
     unit_h = resolution_h / cam_h
 
     while True:
-        # Grab the frame from the threaded video file stream, resize
-        # it, and convert it to grayscale
-        # channels)
+   
         _, frame = vid.read()
         frame = cv2.flip(frame, 1)
         frame = imutils.resize(frame, width=cam_w, height=cam_h)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # Detect faces in the grayscale frame
+       
         rects = detector(gray, 0)
 
-        # Loop over the face detections
         if len(rects) > 0:
             rect = rects[0]
         else:
@@ -77,25 +69,20 @@ def mouse():
             key = cv2.waitKey(1) & 0xFF
             continue
 
-        # Determine the facial landmarks for the face region, then
-        # convert the facial landmark (x, y)-coordinates to a NumPy
-        # array
+       
         shape = predictor(gray, rect)
         shape = face_utils.shape_to_np(shape)
 
-        # Extract the left and right eye coordinates, then use the
-        # coordinates to compute the eye aspect ratio for both eyes
         mouth = shape[mStart:mEnd]
         leftEye = shape[lStart:lEnd]
         rightEye = shape[rStart:rEnd]
         nose = shape[nStart:nEnd]
 
-        # Because I flipped the frame, left is right, right is left.
+       
         temp = leftEye
         leftEye = rightEye
         rightEye = temp
 
-        # Average the mouth aspect ratio together for both eyes
         mar = mouth_aspect_ratio(mouth)
         leftEAR = eye_aspect_ratio(leftEye)
         rightEAR = eye_aspect_ratio(rightEye)
@@ -104,8 +91,6 @@ def mouse():
 
         nose_point = (nose[3, 0], nose[3, 1])
 
-        # Compute the convex hull for the left and right eye, then
-        # visualize each of the eyes
         mouthHull = cv2.convexHull(mouth)
         leftEyeHull = cv2.convexHull(leftEye)
         rightEyeHull = cv2.convexHull(rightEye)
@@ -116,8 +101,6 @@ def mouse():
         for (x, y) in np.concatenate((mouth, leftEye, rightEye), axis=0):
             cv2.circle(frame, (x, y), 2, GREEN_COLOR, -1)
             
-        # Check to see if the eye aspect ratio is below the blink
-        # threshold, and if so, increment the blink frame counter
         if diff_ear > WINK_AR_DIFF_THRESH:
 
             if leftEAR < rightEAR:
@@ -147,7 +130,6 @@ def mouse():
                     # INPUT_MODE = not INPUT_MODE
                     EYE_COUNTER = 0
 
-                    # nose point to draw a bounding box around it
 
             else:
                 EYE_COUNTER = 0
@@ -157,9 +139,9 @@ def mouse():
             MOUTH_COUNTER += 1
 
             if MOUTH_COUNTER >= MOUTH_AR_CONSECUTIVE_FRAMES:
-                # if the alarm is not on, turn it on
+                
                 INPUT_MODE = not INPUT_MODE
-                # SCROLL_MODE = not SCROLL_MODE
+                
                 MOUTH_COUNTER = 0
                 ANCHOR_POINT = nose_point
 
@@ -201,15 +183,14 @@ def mouse():
         cv2.putText(frame, "Left EAR: {:.2f}".format(leftEAR), (460, 130),cv2.FONT_HERSHEY_SIMPLEX, 0.7, YELLOW_COLOR, 2)
         cv2.putText(frame, "Diff EAR: {:.2f}".format(np.abs(leftEAR - rightEAR)), (460, 80),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
-        # Show the frame
+       
         cv2.imshow("Frame", frame)
         key = cv2.waitKey(1) & 0xFF
 
-        # If the `Esc` key was pressed, break from the loop
+      
         if key == 27:
             break
 
-    # Do a bit of cleanup
     cv2.destroyAllWindows()
     vid.release()
 mouse()
