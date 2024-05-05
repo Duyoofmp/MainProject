e7554d511c8ad66c5e764fbfe147a73762f060c5
Duyoofmp <duyoofibnudileep@gmail.com>
2024-05-05 18:32:07 +0530

    mostly last commit

--- Client/src/Components/userAuthComponents/SignUp.jsx
+++ Client/src/Components/userAuthComponents/SignUp.jsx
@@ -3,6 +3,9 @@ import {Link,useNavigate} from 'react-router-dom'
 import { useState } from 'react';
 import { UseRegister } from './AuthHandlers/authHandlers';
 import {Toaster,toast} from 'react-hot-toast';
+import { createPortal } from 'react-dom';
+import KeyboardOn from "../KeyboardOn.jsx";
+
 function SignUp() {
 
     const [name, setName] = useState(null);
@@ -12,7 +15,61 @@ function SignUp() {
     const [password, setPassword] = useState(null);
     const [loader,setloader] = useState(false);
     const navigate = useNavigate();
+    const [emailBarActive, setEmailhBarActive] = useState(false);
+    const [passwordBarActive, setPasswordBarActive] = useState(false);
+    const [nameBarActive, setNameBarActive] = useState(false);
+    const [addressBarActive, setAddressBarActive] = useState(false);
+    const [phoneBarActive, setPhoneBarActive] = useState(false);
 
+    const handleEmailFocus = () => {
+        setEmailhBarActive(true);
+        setPasswordBarActive(false);
+        setPhoneBarActive(false)
+        setAddressBarActive(false)
+        setNameBarActive(false)
+
+
+      };
+      const handlePasswordFocus = () => {
+        setEmailhBarActive(false);
+        setPasswordBarActive(true);
+        setPhoneBarActive(false)
+        setAddressBarActive(false)
+        setNameBarActive(false)
+
+      };
+      const handleNameFocus = () => {
+        setEmailhBarActive(false);
+        setPasswordBarActive(false);
+        setPhoneBarActive(false)
+        setAddressBarActive(false)
+        setNameBarActive(true)
+
+      };
+      const handleAddressFocus = () => {
+        setEmailhBarActive(false);
+        setPasswordBarActive(false);
+        setPhoneBarActive(false)
+        setAddressBarActive(true)
+        setNameBarActive(false)
+
+      };
+      const handlePhoneFocus = () => {
+        setEmailhBarActive(false);
+        setPasswordBarActive(false);
+        setPhoneBarActive(true)
+        setAddressBarActive(false)
+        setNameBarActive(false)
+
+      };
+      const handleSearchBlur = () => {
+        setEmailhBarActive(false);
+        setPasswordBarActive(false);
+        setPhoneBarActive(false)
+        setAddressBarActive(false)
+        setNameBarActive(false)
+
+      };
 
     const SubmitHandler = async(e) => {
 
@@ -68,11 +125,11 @@ function SignUp() {
 
    
     <form onSubmit={SubmitHandler}>
-        <input type="text" required disabled={loader}  name="name" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
-          <input type="text" required disabled={loader}  name="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-          <input type="number" required disabled={loader}  name="phoneNumber" placeholder="PhoneNumber" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
-          <input type="text" required disabled={loader}  name="address" placeholder="Address" value={address} onChange={(e) => setAddress(e.target.value)} />
-          <input type="password" required disabled={loader}  name="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <input type="text" required disabled={loader}  onFocus={handleNameFocus}  name="name" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+          <input type="text" required disabled={loader}  onFocus={handleEmailFocus}  name="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input type="number" required disabled={loader}  onFocus={handlePhoneFocus}  name="phoneNumber" placeholder="PhoneNumber" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+          <input type="text" required disabled={loader}   onFocus={handleAddressFocus} name="address" placeholder="Address" value={address} onChange={(e) => setAddress(e.target.value)} />
+          <input type="password" required disabled={loader}  onFocus={handlePasswordFocus   } name="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
           <button type="submit" required disabled={loader} >{loader ? 'SigningUp...' : 'SignUp'}</button>
         <p>Don't have an account? <Link to={'/login'}>Login</Link></p>
 
@@ -81,6 +138,41 @@ function SignUp() {
     </div>
     
    </div>
+   {emailBarActive && (
+        // Render your component here when search bar is active
+        createPortal(
+          <KeyboardOn  setSearch={setEmail} handleClose={handleSearchBlur} />,
+          document.body
+        )
+      )}
+       {passwordBarActive && (
+        // Render your component here when search bar is active
+        createPortal(
+          <KeyboardOn  setSearch={setPassword} handleClose={handleSearchBlur} />,
+          document.body
+        )
+      )}
+      {nameBarActive && (
+        // Render your component here when search bar is active
+        createPortal(
+          <KeyboardOn  setSearch={setName} handleClose={handleSearchBlur} />,
+          document.body
+        )
+      )}
+       {addressBarActive && (
+        // Render your component here when search bar is active
+        createPortal(
+          <KeyboardOn  setSearch={setAddress} handleClose={handleSearchBlur} />,
+          document.body
+        )
+      )}
+      {phoneBarActive && (
+        // Render your component here when search bar is active
+        createPortal(
+          <KeyboardOn  setSearch={setPhoneNumber} handleClose={handleSearchBlur} />,
+          document.body
+        )
+      )}
    </>
   )
 }
